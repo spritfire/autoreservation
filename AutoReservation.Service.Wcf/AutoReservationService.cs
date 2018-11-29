f5392b11b2d93221fec819e8dce300f533265c7b
Michel Bongard <michel_bongard@hotmail.com>
2018-11-29 11:47:28 +0100

    gitignore problems

--- AutoReservation.Service.Wcf/AutoReservationService.cs
+++ AutoReservation.Service.Wcf/AutoReservationService.cs
@@ -1,12 +1,59 @@
-﻿using System;
+﻿using AutoReservation.BusinessLayer;
+using AutoReservation.Common.DataTransferObjects;
+using AutoReservation.Dal.Entities;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace AutoReservation.Service.Wcf
 {
     public class AutoReservationService
     {
-
         private static void WriteActualMethod()
             => Console.WriteLine($"Calling: {new StackTrace().GetFrame(1).GetMethod().Name}");
+
+        public KundeDto GetKundeById(int kundeId)
+        {
+            WriteActualMethod();
+            return new KundeManager()
+                .GetById(kundeId)
+                .ConvertToDto();
+        }
+
+        public void InsertKunde(KundeDto kunde)
+        {
+            WriteActualMethod();
+            Kunde kundeEntity = kunde.ConvertToEntity();
+            new KundeManager()
+                .Insert(kundeEntity);
+        }
+
+        public void UpdateKunde(KundeDto kunde)
+        {
+            WriteActualMethod();
+            Kunde kundeEntity = kunde.ConvertToEntity();
+            new KundeManager()
+                .Update(kundeEntity);
+        }
+
+        public void RemoveKunde(KundeDto kunde)
+        {
+            WriteActualMethod();
+            Kunde kundeEntity = kunde.ConvertToEntity();
+            new KundeManager()
+                .Remove(kundeEntity);
+        }
+
+        public List<KundeDto> KundeList()
+        {
+            WriteActualMethod();
+            List<KundeDto> kundeDtos = new List<KundeDto>();
+            List<Kunde> kundeEntities = new KundeManager().List();
+            foreach (Kunde k in kundeEntities)
+            {
+                kundeDtos.Add(k.ConvertToDto());
+            }
+            return kundeDtos;
+        }
     }
 }
