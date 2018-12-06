5562517cb6c0223c2e45b38d5dd31f96d41abc52
Kunz Michelle <michelle.kunz@hsr.ch>
2018-12-06 08:28:51 +0100

    Add new exceptions

---
+++ AutoReservation.BusinessLayer/Exceptions/AutoUnavailableException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using AutoReservation.Dal.Entities;
+
+namespace AutoReservation.BusinessLayer.Exceptions
+{
+    public class AutoUnavailableException
+        : Exception
+    {
+        public AutoUnavailableException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ AutoReservation.BusinessLayer/Exceptions/InvalidDateRangeException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AutoReservation.BusinessLayer.Exceptions
+{
+    public class InvalidDateRangeException
+        : Exception
+    {
+        public InvalidDateRangeException(string message) : base(message)
+        {
+        }
+
+        public InvalidDateRangeException(string message, DateTime von, DateTime bis) : base(message)
+        {
+            Von = von;
+            Bis = bis;
+        }
+
+        public DateTime Von { get; set; }
+        public DateTime Bis { get; set; }
+    }
+}
