b91a5ee0920388c147be8598ce2978b2a3c71998
Michel Bongard <michel_bongard@hotmail.com>
2018-12-06 11:24:03 +0100

    missing faults

--- AutoReservation.Common/DataTransferObjects/Faults/OptimisticConcurrencyFault.cs
+++ AutoReservation.Common/DataTransferObjects/Faults/OptimisticConcurrencyFault.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AutoReservation.Common.DataTransferObjects.Faults
 {
-    class OptimisticConcurrencyFault
+    [DataContract]
+    public class OptimisticConcurrencyFault
     {
+        [DataMember]
+        public string Operation { get; set; }
+
+        [DataMember]
+        public string Description { get; set; }
     }
 }
