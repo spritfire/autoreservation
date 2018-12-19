1e3d0b0e40808c60abb4e0a1e6d03857284cf6b8
Michel Bongard <michel_bongard@hotmail.com>
2018-12-19 01:02:39 +0100

    some more tests

--- AutoReservation.Client.ViewModels.Testing/TestUI.cs
+++ AutoReservation.Client.ViewModels.Testing/TestUI.cs
@@ -29,8 +29,8 @@ namespace AutoReservation.Client.Testing
             var query = from w in windows where w.Name.Equals("ReservationDetailWindow") select w;
             Assert.IsFalse(query.Any());
             btnNewReservation.Click();
-            var reservatoinDetailWindow = app.GetWindow("ReservationDetailWindow", InitializeOption.NoCache);
-            Assert.IsNotNull(reservatoinDetailWindow);
+            var reservationDetailWindow = app.GetWindow("ReservationDetailWindow", InitializeOption.NoCache);
+            Assert.IsNotNull(reservationDetailWindow);
             app.Close();
         }
 
@@ -47,8 +47,8 @@ namespace AutoReservation.Client.Testing
             var query = from w in windows where w.Name.Equals("AutoDetailWindow") select w;
             Assert.IsFalse(query.Any());
             btnNewCar.Click();
-            var reservatoinDetailWindow = app.GetWindow("AutoDetailWindow", InitializeOption.NoCache);
-            Assert.IsNotNull(reservatoinDetailWindow);
+            var carDetailWindow = app.GetWindow("AutoDetailWindow", InitializeOption.NoCache);
+            Assert.IsNotNull(carDetailWindow);
             app.Close();
         }
 
@@ -65,8 +65,84 @@ namespace AutoReservation.Client.Testing
             var query = from w in windows where w.Name.Equals("KundeDetailWindow") select w;
             Assert.IsFalse(query.Any());
             btnNewCustomer.Click();
-            var reservatoinDetailWindow = app.GetWindow("KundeDetailWindow", InitializeOption.NoCache);
-            Assert.IsNotNull(reservatoinDetailWindow);
+            var customerDetailWindow = app.GetWindow("KundeDetailWindow", InitializeOption.NoCache);
+            Assert.IsNotNull(customerDetailWindow);
+            app.Close();
+        }
+
+        [TestMethod]
+        public void TestButtonSaveCustomer()
+        {
+            var app = Application.Launch(@"C:\Users\Michel\Desktop\autoreservation\AutoReservation.Client.UI\bin\Debug\AutoReservation.Client.UI.exe");
+            var mainWindow = app.GetWindow("MainWindow", InitializeOption.NoCache);
+            var tabControl = mainWindow.Get<Tab>(SearchCriteria.ByClassName("TabControl"));
+            tabControl.SelectTabPage(2);
+            var btnNewCustomer = mainWindow.Get<Button>(SearchCriteria.ByText("New Customer"));
+            btnNewCustomer.Click();
+            var customerDetailWindow = app.GetWindow("KundeDetailWindow", InitializeOption.NoCache);
+
+            var btnSaveCustomer = customerDetailWindow.Get<Button>(SearchCriteria.ByText("Save"));
+            btnSaveCustomer.Click();
+            var windows = app.GetWindows();
+            var query = from w in windows where w.Name.Equals("KundeDetailWindow") select w;
+            Assert.IsFalse(query.Any());
+            app.Close();
+        }
+
+        [TestMethod]
+        public void TestButtonSaveCar()
+        {
+            var app = Application.Launch(@"C:\Users\Michel\Desktop\autoreservation\AutoReservation.Client.UI\bin\Debug\AutoReservation.Client.UI.exe");
+            var mainWindow = app.GetWindow("MainWindow", InitializeOption.NoCache);
+            var tabControl = mainWindow.Get<Tab>(SearchCriteria.ByClassName("TabControl"));
+            tabControl.SelectTabPage(1);
+            var btnNewCar = mainWindow.Get<Button>(SearchCriteria.ByText("New Car"));
+            btnNewCar.Click();
+            var carDetailWindow = app.GetWindow("AutoDetailWindow", InitializeOption.NoCache);
+
+            var btnSaveCar = carDetailWindow.Get<Button>(SearchCriteria.ByText("Save"));
+            btnSaveCar.Click();
+            var windows = app.GetWindows();
+            var query = from w in windows where w.Name.Equals("AutoDetailWindow") select w;
+            Assert.IsFalse(query.Any());
+            app.Close();
+        }
+
+        [TestMethod]
+        public void TestButtonDeleteCustomer()
+        {
+            var app = Application.Launch(@"C:\Users\Michel\Desktop\autoreservation\AutoReservation.Client.UI\bin\Debug\AutoReservation.Client.UI.exe");
+            var mainWindow = app.GetWindow("MainWindow", InitializeOption.NoCache);
+            var tabControl = mainWindow.Get<Tab>(SearchCriteria.ByClassName("TabControl"));
+            tabControl.SelectTabPage(2);
+            var btnNewCustomer = mainWindow.Get<Button>(SearchCriteria.ByText("New Customer"));
+            btnNewCustomer.Click();
+            var customerDetailWindow = app.GetWindow("KundeDetailWindow", InitializeOption.NoCache);
+
+            var btnSaveCustomer = customerDetailWindow.Get<Button>(SearchCriteria.ByText("Delete Customer"));
+            btnSaveCustomer.Click();
+            var windows = app.GetWindows();
+            var query = from w in windows where w.Name.Equals("KundeDetailWindow") select w;
+            Assert.IsTrue(query.Any());
+            app.Close();
+        }
+
+        [TestMethod]
+        public void TestButtonDeleteCar()
+        {
+            var app = Application.Launch(@"C:\Users\Michel\Desktop\autoreservation\AutoReservation.Client.UI\bin\Debug\AutoReservation.Client.UI.exe");
+            var mainWindow = app.GetWindow("MainWindow", InitializeOption.NoCache);
+            var tabControl = mainWindow.Get<Tab>(SearchCriteria.ByClassName("TabControl"));
+            tabControl.SelectTabPage(1);
+            var btnNewCar = mainWindow.Get<Button>(SearchCriteria.ByText("New Car"));
+            btnNewCar.Click();
+            var carDetailWindow = app.GetWindow("AutoDetailWindow", InitializeOption.NoCache);
+
+            var btnSaveCar = carDetailWindow.Get<Button>(SearchCriteria.ByText("Delete Car"));
+            btnSaveCar.Click();
+            var windows = app.GetWindows();
+            var query = from w in windows where w.Name.Equals("AutoDetailWindow") select w;
+            Assert.IsTrue(query.Any());
             app.Close();
         }
     }
